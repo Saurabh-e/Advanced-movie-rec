1d7eb58f794e65fc0c693b687482e3af2eb1d7cb
Saurabh Pandey <148532842+Saurabh-e@users.noreply.github.com>
2026-04-10 22:02:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ with st.sidebar:
 
     cols = st.slider("Columns", 4, 8, 6)
 
-    year_filter = st.slider("Year", 1980, 2025, (2000, 2025))
+    year_filter = st.slider("Year", 1980, 2025, (2000, 2026))
     rating_filter = st.slider("Rating", 0.0, 10.0, (5.0, 10.0))
 
 # =============================
